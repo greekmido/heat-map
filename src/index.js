d0246d12d0d9a9d2a6e3f2e710d1b2802bb715ec
mohammad alameri <onex911@hotmail.com>
2022-03-17 12:37:08 +0200

    a fully working heat-map

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './index.css';
 import * as d3 from 'd3'
-import { axisBottom } from 'd3';
-import { schemeBrBG } from 'd3';
-
+//setting the size and margin for main svg
 const width = 1300;
 const height = 600;
 const margin = {"left":60,"bottom":30,"right":50,"top":30}
@@ -10,32 +8,45 @@ const margin = {"left":60,"bottom":30,"right":50,"top":30}
 d3.select("body").append("svg").attr("height",height).attr("width",width)
 .attr("x",margin.left).attr("id","plot");
 
+//fetching our data
 fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json")
 .then((response)=>{
     return response.json();
 })
 .then((data)=>{
-    const xScale = d3.scaleBand()
-    .domain(data.monthlyVariance.map(function (val) {
+    //setting the scales
+    const xScale = d3.scaleBand().domain(data.monthlyVariance.map(function (val) {
         return val.year}))
-    .range([margin.left,width-margin.right]);
-    
+         .range([margin.left,width-margin.right]);
     const yScale = d3.scaleBand().domain([1,2,3,4,5,6,7,8,9,10,11,12])
-    .range([height-margin.bottom,margin.top]);
+         .range([height-margin.bottom,margin.top]);  
+    const colorRange = d3.scaleSequential(d3.interpolateSpectral);
+    const colorDomain = [d3.min(data.monthlyVariance,d=>d.variance)
+        ,d3.max(data.monthlyVariance,d=>d.variance)]
+    const varScale = d3.scaleLinear().domain(colorDomain).range([1,0])
 
-    const colScale = d3.scaleQuantize().domain([d3.min(data.monthlyVariance,d=>d.variance)
-    ,d3.max(data.monthlyVariance,d=>d.variance)])
-    .range(["#03AEFD", "#10AEF0", "#1FB0E1", "#37B3C8", "#4DB8B3", 
-    "#6BBE95", "#89C576", "#A1CA60", "#BDD043", "#D1D32F", "#E8D718","#F5D70A","#FFD103","#FFB804","#FFAD03","#FF9C03","#FF9003","#FF7A03","#FF5F03","#FF3B01","#FF2F01","#FF1F01","#FF1501","#FF0900","#FF0200","#CD0000","#980000","#7F0000","#760000"]);
+    //tooltip
+    d3.select("body").append("div").attr("class","tooltip").html("hello tooltip");
 
+    //heating the map
     d3.select("#plot").selectAll("rect").data(data.monthlyVariance).enter()
     .append('rect')
-    .style("fill",d=>colScale(d.variance))
+    .style("fill",d=>colorRange(varScale(d.variance)))
     .attr("x",(d)=>{return xScale(d.year)})
     .attr("y",d=>yScale(d.month))
     .attr('width',xScale.bandwidth())
-    .attr("height",yScale.bandwidth());
+    .attr("height",yScale.bandwidth())
+    .on("mouseover",(e,d)=>{d3.select(".tooltip").html(`year:${d.year}<br>month:${d.month}<br>variance:${d.variance} C`).style("top",e.pageY-80+"px").style("left",e.pageX-50+"px")
+        .style("background-color",`${colorRange(varScale(d.variance))}`).style("padding","5px").style("border-width","1px").style("border-style","solid").transition().style("opacity","1")
+        .style("color",()=>d.variance<-4||d.variance>4?"white":"black")
+        console.log(d)
+    d3.select(e.target).attr("stroke","black")
+    }).on("mouseout",(e,d)=>{
+        d3.select(".tooltip").transition().style("opacity","0")
+        d3.select(e.target).attr("stroke","none")
+    });
 
+    //definning axis
     const xAxis= d3.axisBottom().scale(xScale).tickValues(
         xScale.domain().filter(year=>year % 10 === 0));
 
@@ -57,17 +68,18 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
     .attr("transform",`translate(${margin.left},0)`).call(yAxis);
    
 
-      var legendAxis = d3.scaleLinear().domain(colScale.domain().map((el)=>el+1)).range([0,200]);
+      var legendAxis = d3.scaleLinear().domain(colorDomain.map((el)=>el)).range([0,200]);
+      const testingscale = d3.scaleLinear().domain([0,1]).range([200,0]);
 
-      const xlegendscale = d3.scaleLinear().domain(colScale.domain()).range([schemeBrBG[11]])
+    d3.select("body").append("svg").attr("id","legend").attr("height","50").attr("width","200").append("g").call(d3.axisTop().scale(legendAxis))
+    .attr("transform",`translate(0,19)`);
+    
+    d3.select("#legend").selectAll('rect').data(d3.range(0,1,1/200))
+    .enter().append("rect").attr("x",(d)=>testingscale(d)).attr("y",50-30).attr("width","1").attr("height","20")
+    .attr("fill",(d)=>colorRange(d));
+    
+    d3.select("#legend").append("text").text("C").attr("y",16)
 
-    d3.select("body").append("svg").attr("id","legend").append("g").call(d3.axisBottom().scale(legendAxis))
-    .attr("transform",`translate(0,25)`)
-    
-    
-    d3.select("#legend").selectAll('rect').data(d3.range(d3.min(data.monthlyVariance,(d)=>d.variance),d3.max(data.monthlyVariance,(d)=>d.variance+1),0.1))
-    .enter().append("rect").attr("x",(d)=>legendAxis(d)).attr("y","0").attr("width","2px").attr("height","20")
-    .attr("fill",(d)=>colScale(d))
 })
 
 
